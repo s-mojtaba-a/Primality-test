bc81a1bb1c4b525ec089f9d5ad51ed7eafc61c94
Mojtaba Abedi <68202630+s-mojtaba-a@users.noreply.github.com>
2020-09-30 20:14:00 +0330

    Update Cpp.cpp

--- Cpp.cpp
+++ Cpp.cpp
@@ -4,25 +4,38 @@ typedef long long ll;
 typedef pair<int,int> pii ;
 
 template<typename T>
-bool primality_test(T n){
+bool is_prime(T n){
+    // returns 1 if number is prime
+    // otherwise returns 0
     if (n==2){
         return 1 ;
     }
     if (n%2==0){
-        return 1;
+        return 0;
     }
     T i=3 ;
     while (i*i<=n){
         if (n%i==0){
-            return 1;
+            return 0;
         }
         i+=2;
     }
-    return 0 ;
+    return 1 ;
 }
 
-int main()
-{
-    ios_base::sync_with_stdio(false);
-    cin.tie(0);
-}
+######## python 3.8.2 ########
+
+def is_prime(n):
+    ''' returns 1 if number is prime
+        else 0
+     '''
+    if n==2:
+        return 1
+    if n%2==0 :
+        return 0
+    i=3
+    while (i*i<=n):
+        if n%i==0 :
+            return 0
+        i+=2
+    return 1
